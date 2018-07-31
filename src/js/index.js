73ece8818b960643196eba9df620b9394c7bac2e
jiabinleo <jiabinleo@126.com>
2018-07-31 15:46:01 +0800

    组件化应用构建

--- src/js/index.js
+++ src/js/index.js
@@ -51,3 +51,18 @@ var app6 = new Vue({
     message: "Hello vue!"
   }
 });
+
+Vue.component("todo-item", {
+  props: ["todo"],
+  template: "<li>{{todo.text}}</li>"
+});
+var app7 = new Vue({
+  el: "#app-7",
+  data: {
+    groceryList: [
+      { id: 0, text: "蔬菜" },
+      { id: 1, text: "奶酪" },
+      { id: 2, text: "水果" }
+    ]
+  }
+});
