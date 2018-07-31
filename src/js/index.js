c16a823f3f152c666ffbc2fb1eccad59efd430af
jiabinleo <jiabinleo@126.com>
2018-07-31 16:10:12 +0800

    数据与方法

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-var vm = new Vue({});
+var obj = {
+  foo: "bar"
+};
+Object.freeze(obj);
+
+var vm = new Vue({
+  el: "#app",
+  data: obj
+});
+
+var data = { a: 1 };
+var vm = new Vue({
+  el: "#example",
+  data: data
+});
+console.log(vm.$data === data); //  ==>true
+console.log(vm.$el === document.getElementById("example")); //  ==>true
+
+//$watch是一个实例方法
+vm.$watch("a", function(newValue, oldVvalue) {
+    //这个回调将在 'vm.a' 改变后调用
+});
