3ab3002b3e2d8200b244cfb598d0bde823d29875
jiabinleo <jiabinleo@126.com>
2018-07-31 16:17:36 +0800

    实例生命周期钩子

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,8 @@
-var obj = {
-  foo: "bar"
-};
-Object.freeze(obj);
-
-var vm = new Vue({
-  el: "#app",
-  data: obj
-});
-
-var data = { a: 1 };
-var vm = new Vue({
-  el: "#example",
-  data: data
-});
-console.log(vm.$data === data); //  ==>true
-console.log(vm.$el === document.getElementById("example")); //  ==>true
-
-//$watch是一个实例方法
-vm.$watch("a", function(newValue, oldVvalue) {
-    //这个回调将在 'vm.a' 改变后调用
+new Vue({
+  data: {
+    a: 1
+  },
+  created: function() {
+    console.log("a is" + this.a);
+  }
 });
