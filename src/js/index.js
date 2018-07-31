71a5111804687a016c8610a9e646ae32140eac39
jiabinleo <jiabinleo@126.com>
2018-07-31 15:53:54 +0800

    创建一个Vue实例

--- src/js/index.js
+++ src/js/index.js
@@ -1,68 +1,70 @@
-var app = new Vue({
-  el: "#app",
-  data: {
-    message: "Hello Vue!"
-  }
-});
+// var app = new Vue({
+//   el: "#app",
+//   data: {
+//     message: "Hello Vue!"
+//   }
+// });
 
-var app2 = new Vue({
-  el: "#app-2",
-  data: {
-    message: "页面加载于" + new Date().toLocaleString()
-  }
-});
+// var app2 = new Vue({
+//   el: "#app-2",
+//   data: {
+//     message: "页面加载于" + new Date().toLocaleString()
+//   }
+// });
 
-var app3 = new Vue({
-  el: "#app-3",
-  data: {
-    seen: false
-  }
-});
+// var app3 = new Vue({
+//   el: "#app-3",
+//   data: {
+//     seen: false
+//   }
+// });
 
-var app4 = new Vue({
-  el: "#app-4",
-  data: {
-    todos: [
-      { text: "学习 Javascript" },
-      { text: "学习 Vue" },
-      { text: "整个项目" }
-    ]
-  }
-});
+// var app4 = new Vue({
+//   el: "#app-4",
+//   data: {
+//     todos: [
+//       { text: "学习 Javascript" },
+//       { text: "学习 Vue" },
+//       { text: "整个项目" }
+//     ]
+//   }
+// });
 
-var app5 = new Vue({
-  el: "#app-5",
-  data: {
-    message: "Hello Vue.js!"
-  },
-  methods: {
-    reverseMessage: function() {
-      this.message = this.message
-        .split("")
-        .reverse()
-        .join("");
-    }
-  }
-});
+// var app5 = new Vue({
+//   el: "#app-5",
+//   data: {
+//     message: "Hello Vue.js!"
+//   },
+//   methods: {
+//     reverseMessage: function() {
+//       this.message = this.message
+//         .split("")
+//         .reverse()
+//         .join("");
+//     }
+//   }
+// });
 
-var app6 = new Vue({
-  el: "#app-6",
-  data: {
-    message: "Hello vue!"
-  }
-});
+// var app6 = new Vue({
+//   el: "#app-6",
+//   data: {
+//     message: "Hello vue!"
+//   }
+// });
 
-Vue.component("todo-item", {
-  props: ["todo"],
-  template: "<li>{{todo.text}}</li>"
-});
-var app7 = new Vue({
-  el: "#app-7",
-  data: {
-    groceryList: [
-      { id: 0, text: "蔬菜" },
-      { id: 1, text: "奶酪" },
-      { id: 2, text: "水果" }
-    ]
-  }
-});
+// Vue.component("todo-item", {
+//   props: ["todo"],
+//   template: "<li>{{todo.text}}</li>"
+// });
+// var app7 = new Vue({
+//   el: "#app-7",
+//   data: {
+//     groceryList: [
+//       { id: 0, text: "蔬菜" },
+//       { id: 1, text: "奶酪" },
+//       { id: 2, text: "水果" }
+//     ]
+//   }
+// });
+
+var vm = new Vue({});
