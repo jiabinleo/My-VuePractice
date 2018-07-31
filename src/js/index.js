30522b8b4daf9488e354a0eab9adfff3f4f63349
jiabinleo <jiabinleo@126.com>
2018-07-31 17:06:09 +0800

    参数

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,15 @@ var vm = new Vue({
   data: {
     rawHtml: "<span style='color:red'>This should be red.</span>",
     isButtonDisabled: false, //null undefined false | true
-    number:5,
-    ok:false,
-    message:"Hello Vue！",
-    id:5
+    number: 5,
+    ok: false,
+    message: "Hello Vue！",
+    id: 5,
+    url: "http://www.baidu.com"
+  },
+  methods: {
+    doSomething: function() {
+      this.number += 1;
+    }
   }
 });
