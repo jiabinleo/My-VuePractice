25f5b531fbd29a9db28b2142577eb1cf3d509697
jiabinleo <jiabinleo@126.com>
2018-07-31 16:58:12 +0800

    特性 && 使用JavaScript表达式

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 var vm = new Vue({
-    el:"#app",
-    data:{
-        rawHtml:"<span style='color:red'>This should be red.</span>"
-    }
-})
+  el: "#app",
+  data: {
+    rawHtml: "<span style='color:red'>This should be red.</span>",
+    isButtonDisabled: false, //null undefined false | true
+    number:5,
+    ok:false,
+    message:"Hello Vue！",
+    id:5
+  }
+});
