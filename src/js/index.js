27ff5a536154888a045c053b07c7faf7b3de7b27
jiabinleo <jiabinleo@126.com>
2018-08-01 10:19:54 +0800

    Class 与 Style 绑定

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,10 @@
-var watchExample = new Vue({
-  el: "#watch-example",
+var vm = new Vue({
+  el: "#app",
   data: {
-    question: "",
-    answer: "I cannot give you an answer until you ask a question!"
-  },
-  watch: {
-    question: function(newQuestion, oldQuestion) {
-      this.answer = "Waiting for you to stop typing...";
-      this.debouncedGetAnswer();
-    }
-  },
-  created: function() {
-    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500);
-  },
-  methods: {
-    getAnswer: function() {
-      if (this.question.indexOf("?") === -1) {
-        this.answer = "Questions usually contain a question mark. ;-)";
-        return;
-      }
-      this.answer = "Thinking...";
-      var vm = this;
-      axios.get("https://yesno.wtf/api").then(function(response) {
-        vm.answer = _.capitalize(response.data.answer);
-
-        vm.answer = "Error! Could not reach the API. " + error;
-      });
+    fontSize: 30,
+    classActive: {
+      isActive: true,
+      hasError: false
     }
   }
 });
